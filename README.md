edd0e35cc20e9404106d9e91b598078e73f66050
ertugrul <ertugrul013@hotmail.com>
2019-04-22 21:35:25 +0200

    init push

---
+++ README.md
@@ -0,0 +1,3 @@
+# expenses calculator
+
+> this program is made for me personally to make my monthly expenses calculation easier for me.
---
+++ calculations.py
@@ -0,0 +1,13 @@
+# CLI tool for calculating
+class Calculator():
+    #
+    def MunicipalTaxes(self):
+        return(12 + 12 + 15)
+        pass
+    
+    def (self):
+        pass
+    pass
+
+if __name__ == "__main__":
+    pass
