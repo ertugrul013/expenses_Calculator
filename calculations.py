b555e3a845658ecadde371c8c785fdff0fd5295d
ertugrul <ertugrul013@hotmail.com>
2019-05-27 12:15:12 +0200

    backing up the project

--- calculations.py
+++ calculations.py
@@ -4,8 +4,11 @@ class Calculator():
     with open("taxes.json", "r") as read_file:
         taxes_data = json.load(read_file)
     
-    print(taxes_data["Municipaltaxes"][0]["Noord-Brabant"]["First_dog"])
-    "" ""
-    def MunicipalTaxes(self):
-        return(12 + 12 + 15)
+    def MunicipalTaxes(self,provinces):
+
+        waste = self.taxes_data["Municipaltaxes"][provinces]["waste_charges_single"] / 12
+
+
+
+        return(waste + 12 + 15)
     
